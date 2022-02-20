d56362b0806a0d275a25dfefd6ab1f7eda309ddd
andrewsg3 <2238090S@student.gla.ac.uk>
2022-02-20 14:20:23 +0000

    pi integ

--- software/web/app/camera.py
+++ software/web/app/camera.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy
 
+<<<<<<< HEAD
 try:
 	cap=cv2.VideoCapture(0)
 	print("Opened camera at 0")
@@ -11,6 +12,10 @@ except:
 		print("Opened camera at -1")
 	except:
 		print("Could not open camera at -1")
+=======
+cap=cv2.VideoCapture(0)
+print("Opened camera")
+>>>>>>> 60e4c2c29e3a5fb25079222db2e69af90385a523
 
 font = cv2.FONT_HERSHEY_DUPLEX
 
@@ -26,4 +31,8 @@ def gen_frames():
             ret, buffer = cv2.imencode(".jpg",frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
+<<<<<<< HEAD
             b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n') # Concat frame and show result
+=======
+            b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n') # Concat frame and show result
+>>>>>>> 60e4c2c29e3a5fb25079222db2e69af90385a523
