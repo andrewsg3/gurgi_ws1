5235db7012a141545593d7fc4e604f40238decfe
githublemon3 <2238090S@student.gla.ac.uk>
2022-02-20 13:36:27 +0000

    UX improvments

--- software/web/app/camera.py
+++ software/web/app/camera.py
@@ -4,6 +4,8 @@ import numpy
 cap=cv2.VideoCapture(0)
 print("Opened camera")
 
+font = cv2.FONT_HERSHEY_DUPLEX
+
 def gen_frames():
     while True:
         success, frame = cap.read()
@@ -12,6 +14,7 @@ def gen_frames():
             break
         else:
             frame = cv2.flip(frame, 1)
+            frame = cv2.putText(frame, "GURGIWS Live Feed", (10,460), font, 1, (29,207,242),1,cv2.LINE_AA)
             ret, buffer = cv2.imencode(".jpg",frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
