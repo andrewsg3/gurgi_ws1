b7873ac5a0961f77d1bb07f1ce19e90894389503
andrewsg3 <2238090S@student.gla.ac.uk>
2022-04-14 13:16:11 +0100

    Windvane driver testing. Some setup calibration needed to ensure accuracy of resistance/voltage dict.

--- software/drivers/windvane.py
+++ software/drivers/windvane.py
@@ -3,41 +3,37 @@
 #https://projects.raspberrypi.org/en/projects/build-your-own-weather-station/7
 #https://www.argentdata.com/files/80422_datasheet.pdf
 
-#from gpiozero import MCP3008
+from gpiozero import MCP3008
 import time
 import math
+import numpy as np
 
 class windvane():
     """Windvane class
     """
-    def __init__(self):
+    def __init__(self, r1=1450, vin=3.3):
         # Interfacing
         self.channel = 0 # Channel of ADC from which to read voltage 
-        #self.adc = MCP3008(channel = self.channel)
-        self.r1 = 4700 # The resistor used in the system
-        self.vin = 5 #input voltage
-        
+        self.adc = MCP3008(channel = self.channel)
+        self.r1 = r1 # The resistor used in the system
+        self.vin = float(vin) #input voltage
 
         # Readings
         self.sampletime = 5
-        self.volts = { # Voltage lookup table: known values that should be taken when a 4.7k resistor forms voltage divider with anemometer.
-            0.4: 0.0,
-            1.4: 22.5,
-            1.2: 45.0,
-            2.8: 67.5,
-            2.7: 90.0,
-            2.9: 112.5,
-            2.2: 135.0,
-            2.5: 157.5,
-            1.8: 180.0,
-            2.0: 202.5,
-            0.7: 225.0,
-            0.8: 247.5,
-            0.1: 270.0,
-            0.3: 292.5,
-            0.2: 315.0,
-            0.6: 337.5
-        }
+
+	self.resistances = [33000, 6570, 8200, 891, 1000, 688, 2200, 1410, 3900, 3140, 16000,
+	14120, 120000, 42120, 64900, 21880]
+
+	self.angles = [0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180, 202.5, 225, 247.5, 
+	270, 292.5, 315, 337.5]
+
+	self.voltages = np.zeros(len(self.angles))
+
+	for i in range(len(self.resistances)):
+		self.voltages[i] = round((self.vin * self.resistances[i]) / (self.r1 + self.resistances[i]), 1)
+
+        self.volts = dict(zip(self.voltages, self.angles)) # Reference array for relating a read volage to an angle
+
         self.voltage = 0 # Last voltage read from ADC
         self.d = 0 # Last angle calculated from ADC voltage
         self.angles = [] # stored angles
@@ -45,17 +41,30 @@ class windvane():
     def read(self):
         """Read voltage from ADC and make correction / rounding
         """
-        #self.voltage = round(self.adc.value*3.3, 1)
-        self.lookup_angle()
+        self.voltage = round(self.adc.value*3.3, 1)
+        #self.lookup_angle()
 
     def lookup_angle(self):
         """Compare ADC input to lookup table and corresponding angle
         """
         if not self.voltage in self.volts:
-            print("Unknown value of voltage from aneomemeter. Check resistor configuration." + str(self.voltage) + str(self.volts[self.voltage]))
+            print("Unknown value of voltage from windvane. Check resistor configuration.\nMeasured voltage: " + str(self.voltage))
         else:
-            #print("Match " + str(self.voltage) + str(self.volts[self.voltage]))
+            print("Matched voltage: " + str(self.voltage) + "to table value: " + str(self.volts[self.voltage]))
             self.d = self.volts[self.voltage] # Update angle in deg
+
+    def one_minute(self):
+       	    """sample for one minute and print"""
+	    start_time = time.time()
+            record = []
+	    print("Sampling windvane for 60 seconds")
+	    while time.time() - start_time <= 20:
+		    self.read()
+		    record.append(self.voltage)	    
+		    time.sleep(0.005)
+	    print(record)
+	    print(set(record))
+	    print(len(set(record)))
 
     def sample(self):
         """Function to sample over some period and average
---
+++ software/drivers/wvtest.py
@@ -0,0 +1,12 @@
+from gpiozero import MCP3008
+adc = MCP3008(channel=0)
+count = 0
+values = []
+while True:
+	wind = round(adc.value*3.3,1)
+	if not wind in values:
+		values.append(wind)
+		count+=1
+		print(count)
+	if len(values) >= 15:
+		print(values)
